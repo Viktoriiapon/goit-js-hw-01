1311ddbca1e72801ba7bf811c3d0a2bacda51b9c
Viktoriiapon <viktoriyaponom@gmail.com>
2023-11-13 07:35:17 +0100

    change task-1

--- js/task-3.js
+++ js/task-3.js
@@ -8,3 +8,34 @@ function getElementWidth(content, padding, border) {
 console.log(getElementWidth("50px", "8px", "4px"));
 console.log(getElementWidth("60px", "12px", "8.5px"));
 console.log(getElementWidth("200px", "0px", "0px"));
+
+  
+/*function checkPassword(password) {
+  const correctPassword = "jqueryismyjam";
+  const result = password = correctPassword ? `Access granted` : `Access denied, wrong password!`;
+   return result;
+}
+
+console.log(checkPassword("jqueryismyjao"));*/
+
+const fruit = `nyt`;
+
+switch (fruit) {
+  case `apple`:
+    console.log('Apple selected');
+    break;
+  case 'banana':
+    console.log('Banana selected');
+    break;
+  case 'orange':
+    console.log('Orange selected');
+    break;
+  default:
+    console.log('The fruit is unknown');
+}
+
+
+
+
+
+
